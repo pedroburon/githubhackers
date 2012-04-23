5167e3f9cf3c4293921744b49c26be3b4f15fe17
Pedro Burón <pedro@witoi.com>
2012-04-23 00:36:33 -0300

    added  bootstrap css

--- githubhackers/urls.py
+++ githubhackers/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -19,3 +20,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
